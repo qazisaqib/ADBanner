6782d8d05c2a523fa834e07bff8672140111e619
Qazi Rehman <rehmanqazi55@yahoo.com>
2015-10-30 10:01:06 +0500

    Initial Commit

---
+++ ADBanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADBanner
+//
+//  Created by Qazi on 30/10/2015.
+//  Copyright © 2015 Qazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
